9ad2e194a3407cf115fe52f67d3c661a2ec150e9
Levvonci <lvnci@tutanota.com>
2023-11-02 09:00:48 +0100

    .config/.zshrc

--- .config/lvim/ftplugin/javascript.lua
+++
@@ -1,9 +0,0 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { name = "standardjs" },
-}
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { name = "standardjs" },
-}
--- .config/lvim/ftplugin/toml.lua
+++
@@ -1,4 +0,0 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { name = "taplo" },
-}
